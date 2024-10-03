bce74b911c1cc03f93b0e61f8bc2c894b5ad6dfd
Neko Ayaka <neko@ayaka.moe>
2024-10-04 01:05:37 +0800

    fix: lint
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- utils/lt-code/checksum.ts
+++ utils/lt-code/checksum.ts
@@ -1,30 +1,30 @@
 function generateCRCTable() {
-  const crcTable = new Uint32Array(256);
+  const crcTable = new Uint32Array(256)
   for (let i = 0; i < 256; i++) {
-    let crc = i;
+    let crc = i
     for (let j = 8; j > 0; j--) {
       if (crc & 1) {
-        crc = (crc >>> 1) ^ 0xEDB88320; // Polynomial used in CRC-32
+        crc = (crc >>> 1) ^ 0xEDB88320 // Polynomial used in CRC-32
       }
       else {
-        crc = crc >>> 1;
+        crc = crc >>> 1
       }
     }
-    crcTable[i] = crc >>> 0;
+    crcTable[i] = crc >>> 0
   }
-  return crcTable;
+  return crcTable
 }
-const crcTable = /* @__PURE__ */ generateCRCTable();
+const crcTable = /* @__PURE__ */ generateCRCTable()
 /**
  * Get checksum of the data using CRC32 and XOR with k to ensure uniqueness for different chunking sizes
  */
 
 export function getChecksum(uint8Array: Uint8Array, k: number): number {
-  let crc = 0xFFFFFFFF; // Initial value
+  let crc = 0xFFFFFFFF // Initial value
   for (let i = 0; i < uint8Array.length; i++) {
-    const byte = uint8Array[i]!;
-    crc = (crc >>> 8) ^ crcTable[(crc ^ byte) & 0xFF]!;
+    const byte = uint8Array[i]!
+    crc = (crc >>> 8) ^ crcTable[(crc ^ byte) & 0xFF]!
   }
 
-  return (crc ^ k ^ 0xFFFFFFFF) >>> 0; // Final XOR value and ensure 32-bit unsigned
+  return (crc ^ k ^ 0xFFFFFFFF) >>> 0 // Final XOR value and ensure 32-bit unsigned
 }
