c1aa7839566cb14c7bbf8c5aca1286e62da384b7
Rizumu Ayaka <rizumu@ayaka.moe>
2024-10-04 01:45:53 +0800

    feat: github link

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,14 @@
 <template>
   <main h-full>
     <div flex flex-col gap-4 px-4 pb-4 pt-4>
-      <h1 text-4xl>
-        Qrs
-      </h1>
+      <div flex gap-10>
+        <h1 text-4xl>
+          Qrs
+        </h1>
+        <a href="https://github.com/qqrss/qrs" target="_blank" cursor-pointer text-3xl op50 hover:text-blue hover:op100>
+          <div i-carbon:logo-github />
+        </a>
+      </div>
       <div h-full flex gap-5>
         <NuxtLink op70 hover="text-blue" duration-300 transition="all ease-in-out" to="/" active-class="!op100">
           Transfer
